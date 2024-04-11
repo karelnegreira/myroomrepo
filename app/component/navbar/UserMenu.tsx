3fe32cb6f539c8a862593a7226c0e986f46138d5
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-04-12 05:29:47 +1200

    YourRoom button added in navbar

--- app/component/navbar/UserMenu.tsx
+++ app/component/navbar/UserMenu.tsx
@@ -4,9 +4,10 @@ const UserMenu = () => {
   return (
     <div className="relative">
         <div className="flex flex-row items-center gap-3">
-
+            <div onClick={() => {}} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+                YourRoom your home
+            </div>
         </div>
-
     </div>
   )
 }
