becda363df52c91338e035fd4c25dbe55156e68c
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-04-12 05:45:23 +1200

    AiOutlinedMenu icon added in UserMenu navbar component

--- app/component/navbar/UserMenu.tsx
+++ app/component/navbar/UserMenu.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { AiOutlineMenu } from "react-icons/ai";
+
 const UserMenu = () => {
   return (
     <div className="relative">
@@ -8,7 +10,7 @@ const UserMenu = () => {
                 YourRoom your home
             </div>
             <div onClick={() => {}} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
-                
+                <AiOutlineMenu />
             </div>
         </div>
     </div>
