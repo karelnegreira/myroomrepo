f4f0d29307907378876e9d5efb943717cf0b6fbc
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-05-13 23:25:50 +1200

    label click effect added in register modal

--- app/component/inputs/Input.tsx
+++ app/component/inputs/Input.tsx
@@ -70,6 +70,10 @@ const Input: React.FC<InputProps> = ({
                     z-10 
                     origin-[0] 
                     ${formatPrice ? 'left-9' : 'left-4'}
+                    peer-placeholder-shown:scale-100
+                    peer-placeholder-shown:translate-y-0
+                    peer-focus:scale-75
+                    peer-focus:-translate-y-4
                 `}
             >
                 {label}
