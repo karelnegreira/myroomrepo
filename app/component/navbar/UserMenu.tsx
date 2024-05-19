3eb77be9918867ab4434070009946aee4ded7fd1
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-05-20 01:08:49 +1200

    avatar uodpate functionality works the avatar image gets updated depending on the user profile image github only for now

--- app/component/navbar/UserMenu.tsx
+++ app/component/navbar/UserMenu.tsx
@@ -6,7 +6,7 @@ import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
-import { User } from "@prisma/client";
+
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/app/types";
 
@@ -33,7 +33,7 @@ const UserMenu: React.FC<UserMenuProps> = ({currentUser}) => {
             <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
                 <AiOutlineMenu />
                 <div className="hidden md:block">
-                    <Avatar />
+                    <Avatar src={currentUser?.image}/>
                 </div>
             </div>
         </div>
