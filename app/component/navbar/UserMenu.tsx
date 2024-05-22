9886b146ffd0942b0eba6b9e8529446af36fa8dd
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-05-23 06:57:18 +1200

    useRentModal hook and RentModal component were created

--- app/component/navbar/UserMenu.tsx
+++ app/component/navbar/UserMenu.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import { AiOutlineMenu } from "react-icons/ai";
+
 import Avatar from "../Avatar";
-import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { SafeUser } from "@/app/types";
 
 import { signOut } from "next-auth/react";
-import { SafeUser } from "@/app/types";
+import { AiOutlineMenu } from "react-icons/ai";
+import { useCallback, useState } from "react";
 
 interface UserMenuProps {
     currentUser?: SafeUser | null;
@@ -24,10 +25,19 @@ const UserMenu: React.FC<UserMenuProps> = ({currentUser}) => {
         setIsOpen((value) => !value);
     }, []);
 
+    const onRent = useCallback(() => {
+        if (!currentUser) {
+            return loginModal.onOpen();
+        }
+
+        //open  rent modal
+
+    }, [currentUser, loginModal])
+
   return (
     <div className="relative">
         <div className="flex flex-row items-center gap-3">
-            <div onClick={() => {}} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+            <div onClick={onRent} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
                 YourRoom your home
             </div>
             <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
