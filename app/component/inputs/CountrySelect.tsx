c8412aa9a830a4900d5a1bb596ebc85176a5e6fb
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-05-25 05:41:03 +1200

    displaying the list of countries

--- app/component/inputs/CountrySelect.tsx
+++ app/component/inputs/CountrySelect.tsx
@@ -1,9 +1,30 @@
 "use client";
 
-const CountrySelect = () => {
+import useCountries from '@/app/hooks/useCountries';
+import Select from 'react-select';
+
+export type CountrySelectValue = {
+    flag: string;
+    label: string;
+    latlng: Number[];
+    region: string;
+    value: string;
+}
+
+interface CountrySelectProps {
+    value?: CountrySelectValue;
+    onChange: (value: CountrySelectValue) => void;
+}
+
+const CountrySelect: React.FC<CountrySelectProps> = ({value, onChange}) => {
+    const {getAll} = useCountries();
   return (
     <div>
-        
+        <Select
+            placeholder="Anywhere"
+            isClearable
+            options={getAll()}
+        />
     </div>
   )
 }
