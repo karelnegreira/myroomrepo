765a6ee4170e61aaa1d16644c36e100b851f6cb4
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-06-10 01:25:19 +1200

    EmptyState component created

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
 import Image from "next/image";
+import ClientOnly from "./component/ClientOnly";
+import Container from "./component/Container";
+import EmptyState from "./component/EmptyState";
 
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    )
+  }
+
   return (
-   <div className="text-rose-600 text-2xl">Hello Airbnb!</div>
+   <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+            <div>My future listing</div>
+        </div>
+      </Container>
+   </ClientOnly>
   );
 }
