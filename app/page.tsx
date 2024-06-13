e91c7c0d697b070bce10c599a46f1039c29b8b23
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-06-14 02:35:50 +1200

    date-fns package installed

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import Container from "./component/Container";
 import EmptyState from "./component/EmptyState";
 import getListings from "./actions/getListings";
 import ListingCard from "./component/listing/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
@@ -22,7 +24,11 @@ export default async function Home() {
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
             {listings.map((listing: any) => {
               return (
-                <ListingCard key={listing.id} data={listing}/>
+                <ListingCard 
+                  currentUser={currentUser}
+                  key={listing.id} 
+                  data={listing}
+                />
               )
             })}
         </div>
