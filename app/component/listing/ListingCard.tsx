bac0da3a8be4447f26a83e84aecac32cf0209878
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-06-14 02:56:53 +1200

    Image pic is now visible in ListingCard compoenent

--- app/component/listing/ListingCard.tsx
+++ app/component/listing/ListingCard.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import {format} from 'date-fns';
+
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client"
 import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
+import Image from 'next/image';
+
 
 
 interface ListingCardProps {
@@ -55,12 +59,33 @@ const ListingCard: React.FC<ListingCardProps> = ({
 
         const start = new Date(reservation.startDate);
         const end = new Date(reservation.endDate);
-        
 
-    }, [])
+        return `${format(start, 'PP')} - ${format(end, 'PP')}`;
+
+    }, [reservation])
 
   return (
-    <div>ListingCard</div>
+    <div 
+        onClick={() => router.push( `/listings/${data.id}`)}
+        className="col-span-1 cursor-pointer group">
+            <div className="flex flex-col gap-2 w-full">
+                <div className="
+                    aspect-square
+                    w-full
+                    relative
+                    overflow-hidden
+                    rounded-xl
+                ">
+                    <Image 
+                        fill
+                        alt="Listing"
+                        src={data.imageSrc}
+                        className="object-cover h-full w-full group-hover:scale-110 transition"
+                    />
+                </div>
+            </div>
+
+    </div>
   )
 }
 
