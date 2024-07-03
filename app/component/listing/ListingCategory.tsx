fe0eb94fb837bc61a9b272af6d9b3067d3ab5539
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-07-03 22:58:47 +1200

    ListingCategory.tsx created

---
+++ app/component/listing/ListingCategory.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const ListingCategory = () => {
+  return (
+    <div>ListingCategory</div>
+  )
+}
+
+export default ListingCategory
--- app/component/listing/ListingInfo.tsx
+++ app/component/listing/ListingInfo.tsx
@@ -5,6 +5,7 @@ import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
 import { IconType } from "react-icons";
 import Avatar from "../Avatar";
+import ListingCategory from "./ListingCategory";
 
 interface ListingInfoProps {
     user: SafeUser;
@@ -35,6 +36,7 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
 
   return (
     <div className="col-span-4 flex flex-col gap-8">
+        <div className="flex flex-col gap-2">
         <div className="text-xl font-semibold flex flex-row items-center gap-2">
             <div>Hosted by {user?.name}</div>
             <Avatar src={user?.image}/>
@@ -46,7 +48,19 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
             <div>
                 {roomCount} rooms
             </div>
-        </div>
+            <div>
+                {bathroomCount} bathrooms
+            </div>
+         </div>
+      </div>
+      <hr />
+      {category && (
+        <ListingCategory 
+            icon={category.icon}
+            label={category.label}
+            description={category.description}
+        />
+      )}
     </div>
   )
 }
