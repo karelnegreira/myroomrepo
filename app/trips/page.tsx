19d7c3769a3f4131b9f4a35c424c1f0c3096eee2
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-07-07 21:22:54 +1200

    /trips route added w page.tsx file created and coded TripsClient.tsx added not yet implemented

---
+++ app/trips/page.tsx
@@ -0,0 +1,42 @@
+import EmptyState from '../component/EmptyState';
+import ClientOnly from '../component/ClientOnly'
+
+import getCurrentUser from '../actions/getCurrentUser';
+import getReservations from '../actions/getReservations';
+import TripsClient from './TripsClient';
+
+const TripsPage = async () => {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+        return (
+            <ClientOnly>
+                <EmptyState title="unauthorized" subtitle='Please log in'/>
+            </ClientOnly>
+        );
+    }
+
+    const reservations = await getReservations({
+        userId: currentUser.id
+    });
+
+    if (reservations.length === 0) {
+        return (
+            <ClientOnly>
+                <EmptyState
+                    title="No trips found"
+                    subtitle='It seems you havent booked any trip'
+                />
+            </ClientOnly>
+        )
+    }
+
+    return (
+        <ClientOnly>
+            <TripsClient 
+                reservations={reservations}
+                currentUser={currentUser}
+            />
+        </ClientOnly>
+    )
+}
