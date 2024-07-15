054f4f88fcb7ac6a83a4ec0febe76149a40c795e
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-07-16 02:31:03 +1200

    Listing of favorites ready

--- app/favorites/FavoriteClient.tsx
+++ app/favorites/FavoriteClient.tsx
@@ -1,8 +1,40 @@
+import Container from "../component/Container";
+import Heading from "../component/Heading";
+import ListingCard from "../component/listing/ListingCard";
+import { SafeListing, SafeUser } from "../types"
 
+interface FavoritesClientProps {
+    listings: SafeListing[];
+    currentUser?: SafeUser | null;
+}
 
-const FavoriteClient = () => {
+const FavoriteClient: React.FC<FavoritesClientProps> = ({
+    listings, 
+    currentUser
+}) => {
   return (
-    <div>FavoriteClient</div>
+    <Container>
+        <Heading title="Favorites" subtitle="List of your favorite places!" />
+        <div className="
+            mt-10
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+        ">
+            {listings.map( (listing) => (
+                <ListingCard
+                    currentUser={currentUser}
+                    key={listing.id}
+                    data={listing}
+                />
+            ))}
+        </div>
+    </Container>
   )
 }
 
