b8d7935e387cb3a2e03b44753999c525f6e652e8
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-07-21 20:59:52 +1200

    SearchModal steps state and enum added

--- app/component/modals/SearchModal.tsx
+++ app/component/modals/SearchModal.tsx
@@ -1,11 +1,22 @@
-
+"use client";
 
 import useSearchModal from "@/app/hooks/useSearchModal";
 import Modal from "./Modal";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
+enum STEPS {
+  location = 0, 
+  DATE = 1, 
+  INFO = 2
+}
 
 const SearchModal = () => {
     const searchModal = useSearchModal();
+    const router = useRouter();
+    const params = useSearchParams();
+
+    const [step, nextStep] = useState(STEPS.location); 
 
   return (
     <Modal 
