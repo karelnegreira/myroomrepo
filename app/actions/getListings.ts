3316527588cd28349c86fa013ee35a8f3aeb447a
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-07-24 09:42:22 +1200

    filters implemented

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -14,13 +14,58 @@ export interface IListingParams {
 
 export default async function getListings(params: IListingParams) {
     try {
-        
+
         const { userId, roomCount, guestCount, bathroomCount, locationValue, startDate, endDate, category } = params;
 
         let query: any = {};
 
         if (userId) {
             query.userId = userId;
+        }
+
+        if (category) {
+            query.category = category;
+        }
+
+        if (roomCount) {
+            query.roomCount = {
+                gte: +roomCount
+            }
+        }
+
+        if (bathroomCount) {
+            query.roomCount = {
+                gte: +bathroomCount
+            }
+        }
+
+        if (guestCount) {
+            query.roomCount = {
+                gte: +guestCount
+            }
+        }
+
+        if (locationValue) {
+            query.locationValue = locationValue;
+        }
+
+        if (startDate && endDate) {
+            query.NOT = {
+                reservations: {
+                    some: {
+                        OR: [
+                            {
+                                endDate: { gte: startDate }, 
+                                startDate: { lte: startDate }, 
+                            }, 
+                            {
+                                startDate: { lte: endDate }, 
+                                endDate: { gte: endDate }
+                            }
+                        ]
+                    }
+                }
+            }
         }
 
         const listings = await prisma.listing.findMany({
